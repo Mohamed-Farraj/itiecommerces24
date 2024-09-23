514e37bb2a6ab53f7f95554d8a86d575a433175a
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-09-23 19:22:59 +0300

    cats by routes

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,23 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Iproduct } from '../../core/interfaces/iproduct';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, NgStyle } from '@angular/common';
 import { ApiService } from '../../core/services/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterLink,CurrencyPipe],
+  imports: [RouterLink,CurrencyPipe,NgStyle,FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent implements OnInit{
 
   private readonly _ApiService = inject(ApiService)
+  private readonly _ActivatedRoute = inject(ActivatedRoute)
 
   allProducts:Iproduct[] = []
   filteredProducts:Iproduct[] = []
   categories:any[] = []
+  cat:string ='';
+  selectedCategory: string = '';
 
   ngOnInit(): void {
     this._ApiService.Get_All_Products().subscribe({
@@ -25,15 +29,31 @@ export class ProductsComponent implements OnInit{
         console.log(res);
         this.allProducts = res
         this.filteredProducts = this.allProducts
-      }
-    })
-
-    this._ApiService.Get_Categories().subscribe({
+        this._ApiService.Get_Categories().subscribe({
       next:(res)=>{
         console.log(res);
         this.categories = res
+        this._ActivatedRoute.params.subscribe((params)=>{
+      this.cat = params['cat']
+      this.selectedCategory = this.cat
+      this.filterByCategory();
+      console.log(this.cat);
+    })
       }
     })
+      }
+    })
+
+  }
+
+  
+  filterByCategory(){
+    if(this.cat != 'all')
+    this.filteredProducts = this.filteredProducts.filter(product => product.category.name === this.cat)
+  }
+
+  onCategoryChange(newCategory: string) {
+    
   }
 
 }
