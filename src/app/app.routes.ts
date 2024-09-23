1df4cdfdeed293a61109930f23a6aa6ab60e8767
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-09-24 02:20:34 +0300

    make route to details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { CategoryComponent } from './components/category/category.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:'full'},
     {path:'home' , component:HomeComponent},
     {path:'products/:cat',component:ProductsComponent},
+    {path:'details/:id',component:DetailsComponent},
     {path:'category' , component:CategoryComponent},
     {path: 'admin' , component:AdminComponent},
     {path:'**', redirectTo:'home', pathMatch:'full'} ,
