5dc2e55b0d7d7799c68667b681cecc2089632e76
KaramSaleh333 <karam.saleh333@gmail.com>
2024-09-23 04:01:51 +0300

    Api CRUD and Category handl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductsComponent } from './components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'products/:cat',component:ProductsComponent},
+    {path:'category' , component:CategoryComponent},
+];
