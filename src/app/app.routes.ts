a5e4bb6a434ab1ef132d99958ee206ef4e646da5
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-09-23 03:30:56 +0300

    base design for product card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { ProductsComponent } from './components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'products/:cat',component:ProductsComponent}
+];
