9ae4e86df600ae59bd427208c2ca023e76fe365b
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-09-24 00:31:53 +0300

    update path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'home' , component:HomeComponent},
     {path:'products/:cat',component:ProductsComponent},
     {path:'category' , component:CategoryComponent},
     {path: 'admin' , component:AdminComponent},
+    {path:'**', redirectTo:'home', pathMatch:'full'} ,
 ];
