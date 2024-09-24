59889743ccd3ebaf2f5de0a319bfae892999f233
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-09-24 23:15:29 +0300

    adding slider

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { ApiService } from '../../core/services/api.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  private readonly _ApiService = inject(ApiService)
+  categories:any
+
+
+  ngOnInit(): void {
+    this._ApiService.Get_Categories().subscribe({
+      next: (categories) => {
+        this.categories = categories
+      }
+    })
+    
+  }
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 999,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:false,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 3
+      },
+      740: {
+        items: 4
+      },
+      940: {
+        items: 5
+      }
+    },
+    nav: false
+  }
 
 }
