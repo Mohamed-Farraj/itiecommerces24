fd53230a4d70fbc40fbc1ec8935522ee876f07e1
FatmaHaggag <fatmamohamed3890@gmail.com>
2024-09-24 05:29:04 +0300

    adding dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AdminComponent } from './components/admin/admin.component';
-//import { AddProductComponent } from './components/add-product/add-product.component';
-//import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
 
@@ -15,4 +15,7 @@ export const routes: Routes = [
     {path:'products/:cat',component:ProductsComponent},
     {path:'category' , component:CategoryComponent},
     {path: 'admin' , component:AdminComponent},
+    {path:'details/:id',component:DetailsComponent},
+    {path: 'admin/:id' , component:AddProductComponent},
+    {path:'**' , component:NotFoundComponent} ,
 ];
