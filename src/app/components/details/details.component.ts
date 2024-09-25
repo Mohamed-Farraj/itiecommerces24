321eed18b57beea23fda73aec95c8e97a5017d1e
FatmaHaggag <fatmamohamed3890@gmail.com>
2024-09-25 05:05:41 +0300

    add details component

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../core/services/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-
-}
+  productId: any;
+  myproduct: any;
+  constructor(
+    public service:ApiService,
+    public activatedroute: ActivatedRoute,
+    public router: Router
+  ) {
+    this.productId = this.activatedroute.snapshot.params['id'];
+  }
+  ngOnInit(): void {
+    this.service.get_product(this.productId).subscribe({
+      next: (res) => {
+        this.myproduct = res
+      }
+    })
+  }
+  showAlert(){
+    Swal.fire({
+      title: 'Thank You!',
+      text: 'Your Order On Deliver',
+      icon: 'success',
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#30d633',  // Custom button color
+      cancelButtonColor: '#d33',
+      showCancelButton: false,
+      cancelButtonText: 'Cancel'
+    });
+  }
+ }
