e79c9045fcf046c5cd680504abbabbcfbcd74974
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-09-28 06:17:00 +0300

    add product slider

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from '../../core/services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { Iproduct } from '../../core/interfaces/iproduct';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CarouselModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
   productId: any;
-  myproduct: any;
+  myproduct: Iproduct = {} as Iproduct;
   constructor(
     public service:ApiService,
     public activatedroute: ActivatedRoute,
@@ -21,6 +23,26 @@ export class DetailsComponent {
   ) {
     this.productId = this.activatedroute.snapshot.params['id'];
   }
+
+  
+    customOptions: OwlOptions = {
+      loop: true,
+      margin: 10,
+      mouseDrag: true,
+      touchDrag: true,
+      pullDrag: false,
+      dots: false,
+      navSpeed: 700,
+      navText: ['', ''],
+      items:4,
+      nav: false
+    }
+
+    changeImage(src:string){
+      this.myproduct.imageCover = src
+    }
+
+
   ngOnInit(): void {
     this.service.get_product(this.productId).subscribe({
       next: (res) => {
